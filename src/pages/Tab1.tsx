8dcb97e4a491ea10e5fe08fc6f92dd4ee663e9d3
Dillon Polley <65629239+ESSODillon@users.noreply.github.com>
2022-04-04 08:36:58 -0400

    AR Update

--- src/pages/Tab1.tsx
+++ src/pages/Tab1.tsx
@@ -18,9 +18,12 @@ const Tab1: React.FC = () => {
       </IonHeader>
       <IonContent fullscreen>
       <IframeResizer
-        src="https://www.google.com/maps/embed/v1/place?q=7408%20Constitution%20Ave%2C%20Cedar%20Lake%2C%20IN%2046303&key=..."
+        src="https://mywebar.com/p/Armchair4745"
         title="CLHA | Volunteer"
         frameBorder="0"
+        width="340" 
+        height="600" 
+        allow="camera; accelerometer; vr"
         allowFullScreen
         style={styleWebformIframeEmbed}
       />
