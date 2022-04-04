ed6c3a1f36e038793d9b44cc97d40a25b037a32f
Cappelletti, Jaden Mariano <jamcapp@iu.edu>
2022-04-03 20:22:11 -0400

    Donation Page Design
    Started adding content to the donation page and started designing it.

--- src/pages/Tab3.tsx
+++ src/pages/Tab3.tsx
@@ -1,4 +1,4 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonButton, IonIcon } from '@ionic/react';
+import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonButton, IonIcon, IonText } from '@ionic/react';
 import { logoPaypal } from 'ionicons/icons';
 import './Tab3.css';
 
@@ -10,8 +10,12 @@ const Tab3: React.FC = () => {
           <IonTitle>CLHA | Donate</IonTitle>
         </IonToolbar>
       </IonHeader>
-      <IonContent>
-      <IonButton href='https://www.paypal.com/donate/?hosted_button_id=GXRQVNFFU49UQ' color="warning"><IonIcon slot="start" icon={logoPaypal} /> Donate to CLHA</IonButton>
+      <IonContent color='medium'>
+        <IonText color=''><h1>Donate with PayPal</h1></IonText>
+        <IonText color=''>
+          <p>Every donation helps us present the relevance of and tell the story of Cedar Lake. Make a one-time or recurring gift with this secure PayPal link.</p>
+        </IonText>
+        <IonButton href='https://www.paypal.com/donate/?hosted_button_id=GXRQVNFFU49UQ' color="secondary"><IonIcon slot="start" icon={logoPaypal} /> Donate </IonButton>
     </IonContent>
     </IonPage>
   );
