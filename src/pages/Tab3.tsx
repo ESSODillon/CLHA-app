1ed01adf2e33c4ead8e78373e801f26ca05c3a49
Cappelletti, Jaden Mariano <jamcapp@iu.edu>
2022-04-12 22:00:40 -0400

    Styled Donation Page
    Added styling to the donation page.

--- src/pages/Tab3.tsx
+++ src/pages/Tab3.tsx
@@ -11,11 +11,17 @@ const Tab3: React.FC = () => {
         </IonToolbar>
       </IonHeader>
       <IonContent color='medium'>
-        <IonText color=''><h1>Donate with PayPal</h1></IonText>
-        <IonText color=''>
-          <p>Every donation helps us present the relevance of and tell the story of Cedar Lake. Make a one-time or recurring gift with this secure PayPal link.</p>
-        </IonText>
-        <IonButton href='https://www.paypal.com/donate/?hosted_button_id=GXRQVNFFU49UQ' color="secondary"><IonIcon slot="start" icon={logoPaypal} /> Donate </IonButton>
+        <div className='donateWrapper'>
+          <IonText>
+            <h1>Be a part of history</h1>
+            <p>Help us present the relevvance of Cedar Lake</p>
+            <h1>Donate with PayPal</h1>
+          </IonText>
+          <IonText>
+            <p>Every donation helps us present the relevance of and tell the story of Cedar Lake. Make a one-time or recurring gift with this secure PayPal link.</p>
+          </IonText>
+          <IonButton className='donateBtn' href='https://www.paypal.com/donate/?hosted_button_id=GXRQVNFFU49UQ' color="secondary"><IonIcon slot="start" icon={logoPaypal} /> Donate </IonButton>
+        </div>
     </IonContent>
     </IonPage>
   );
