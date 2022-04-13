611d745073769f6fc052604574ca55e6b0f061a1
Dillon Polley <65629239+ESSODillon@users.noreply.github.com>
2022-04-13 17:24:10 -0400

    Moving the AR Demo

---
+++ src/pages/AR.tsx
@@ -0,0 +1,35 @@
+import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import './Tab1.css';
+import IframeResizer from 'iframe-resizer-react';
+
+const styleWebformIframeEmbed: React.CSSProperties = {
+  width: '1px',
+  minWidth: '100%',
+  height: '100%'
+};
+
+const AR: React.FC = () => {
+  return (
+    <IonPage className=''>
+      <IonHeader>
+        <IonToolbar>
+          <IonTitle>CLHA | AR Demo</IonTitle>
+        </IonToolbar>
+      </IonHeader>
+      <IonContent fullscreen>
+      <IframeResizer
+        src="https://mywebar.com/p/Armchair4745"
+        title="AR Demo"
+        frameBorder="0"
+        width="340" 
+        height="600" 
+        allow="camera; accelerometer; vr"
+        allowFullScreen
+        style={styleWebformIframeEmbed}
+      />
+    </IonContent>
+    </IonPage>
+  );
+};
+
+export default AR;
--- src/pages/Tab1.tsx
+++ src/pages/Tab1.tsx
@@ -1,6 +1,5 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import { IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
 import './Tab1.css';
-import IframeResizer from 'iframe-resizer-react';
 
 const styleWebformIframeEmbed: React.CSSProperties = {
   width: '1px',
@@ -16,18 +15,6 @@ const Tab1: React.FC = () => {
           <IonTitle>CLHA | Map</IonTitle>
         </IonToolbar>
       </IonHeader>
-      <IonContent fullscreen>
-      <IframeResizer
-        src="https://mywebar.com/p/Armchair4745"
-        title="AR Demo"
-        frameBorder="0"
-        width="340" 
-        height="600" 
-        allow="camera; accelerometer; vr"
-        allowFullScreen
-        style={styleWebformIframeEmbed}
-      />
-    </IonContent>
     </IonPage>
   );
 };
